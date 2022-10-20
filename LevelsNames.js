5f8b74a5dc91cb5802b36dfe5c4d134ba5d10712
tyty-04 <tylerseisner@gmail.com>
2022-10-20 10:12:42 +0300

    Everything

---
+++ LevelsNames.js
@@ -0,0 +1,45 @@
+//Set of 4: Ronaldo, Ozil, Smalling, Neymar
+//Correct player: Di Maria
+
+export const playerSets = [
+  {
+    correctPlayer: "Higuain",
+    player1: "Hazard",
+    player1Image:
+      "https://img.a.transfermarkt.technology/portrait/big/50202-1665067742.jpg?lm=1",
+      player1Team: "Chelsea",
+    player2: "Ramsey",
+    player2Image:
+      "https://img.a.transfermarkt.technology/portrait/big/50057-1518795716.jpg?lm=1",
+      player2Team: "Juventus",
+    player3: "Callejon",
+    player3Image:
+      "https://img.a.transfermarkt.technology/portrait/big/61253-1602070711.jpg?lm=1",
+      player3Team: "Napoli",
+    player4: "Ronaldo",
+    player4Image:
+      "https://img.a.transfermarkt.technology/portrait/big/8198-1631656078.jpg?lm=1",
+      player4Team: "Real Madrid",
+  },
+  {
+    correctPlayer: "Di Maria",
+    player1: "Ronaldo",
+    player1Image:
+      "https://img.a.transfermarkt.technology/portrait/big/8198-1631656078.jpg?lm=1",
+      player1Team: "Real Madrid",
+    player2: "Ozil",
+    player2Image:
+      "https://img.a.transfermarkt.technology/portrait/big/35664-1622641562.jpg?lm=1",
+      player2Team: "Real Madrid",
+    player3: "Smalling",
+    player3Image:
+      "https://img.a.transfermarkt.technology/portrait/big/103427-1661351602.jpg?lm=1",
+      player3Team: "Manchester United",
+    player4: "Neymar",
+    player4Image:
+      "https://img.a.transfermarkt.technology/portrait/big/68290-1632687616.jpg?lm=1",
+      player4Team: "PSG"
+  },
+];
+
+export const correctPlayers = ["Higuain", "Di Maria"];
